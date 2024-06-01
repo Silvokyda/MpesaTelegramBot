99bfdf9e7870823e97ef331909100448d011f451
Silvokyda <deseve48@gmail.com>
2024-06-01 09:16:07 +0300

    port

--- bot.py
+++ bot.py
@@ -152,4 +152,5 @@ def main():
 if __name__ == '__main__':
     main()
     logger.info("Starting Flask server...")
-    app.run(port=8000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
